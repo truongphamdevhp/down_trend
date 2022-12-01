a49590be5dc78a99da5c6f5c97d99ed246625b75
truongphamdevhp <truongphamhp92@gmail.com>
2022-12-01 13:35:34 +0700

    test

--- down_trend/down_trend/down_trend.cpp
+++ down_trend/down_trend/down_trend.cpp
@@ -25,8 +25,13 @@ double xa_bo(int so_phien, double phan_tram_toi_da, double gia_hien_tai)
 int so_phien_de_ve_bo(double gia_mua, double gia_hien_tai, double d_max_percent)
 {
 	int nRet = 0;
-	while (std::round(ve_bo(nRet, d_max_percent, gia_hien_tai)) < std::round(gia_mua))
+	double dVal = ve_bo(nRet, d_max_percent, gia_hien_tai);
+	while (std::round(dVal) < std::round(gia_mua))
+	{
 		nRet++;
+		dVal = ve_bo(nRet, d_max_percent, gia_hien_tai);
+	}
+	std::cout << "so tien le " << dVal;
 	return nRet;
 }
 
@@ -40,7 +45,7 @@ int so_phien_xa_bo(double gia_mua, double gia_hien_tai, double d_max_percent)
 
 int main()
 {
-	int nSophienvebo = so_phien_de_ve_bo(34., 20.8, 6.7);
+	int nSophienvebo = so_phien_de_ve_bo(36, 20, 9.9);
 	std::cout << nSophienvebo << "\n";
 	int nSoThang = std::ceil(nSophienvebo / 20.0);
 	return 0;
